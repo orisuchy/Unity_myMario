013a949082b33dc533454f2eeee584f2a215386a
Ori Suchy <orisuchy@gmail.com>
2021-04-27 21:52:05 +0300

    moving to levels

--- Assets/Scripts/CastleScript.cs
+++ Assets/Scripts/CastleScript.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class CastleScript : MonoBehaviour
 {
+    public int level;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
-            Application.LoadLevel("Level2");
+            if (level == 1)
+            {
+                Application.LoadLevel("Level2");
+            }
+           else if (level == 2)
+            {
+                Application.LoadLevel("Win");
+            }
         }
     }
 }
